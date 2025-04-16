021cccdae1068abde996ce223698901385604f65
José Gallardo <tory002254@gmail.com>
2025-04-16 12:52:58 +0200

    Inject header and menu into ConnexionPage; update layout for better structure

--- app/src/main/java/com/example/newvpn/config/ConnexionPage.java
+++ app/src/main/java/com/example/newvpn/config/ConnexionPage.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.newvpn.R;
+import com.example.newvpn.utils.HeaderHelper;
+import com.example.newvpn.utils.MenuHelper;
 
 public class ConnexionPage extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class ConnexionPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_connexion);
+        HeaderHelper.injectHeader(this, R.id.header_container, getString(R.string.connexion_title));
+        MenuHelper.injectMenu(this, R.id.fl_mainpage_menu_container);
     }
 }
