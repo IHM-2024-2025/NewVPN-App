baa0aa4cdaf0f516664287834533683c0b0da16f
José Gallardo <tory002254@gmail.com>
2025-04-17 12:12:31 +0200

    Inject header into LoginPage and RegisterPage for consistent navigation; add click listener for registration

--- app/src/main/java/com/example/newvpn/session/LoginPage.java
+++ app/src/main/java/com/example/newvpn/session/LoginPage.java
@@ -3,10 +3,13 @@ package com.example.newvpn.session;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.newvpn.R;
 import com.example.newvpn.app.MainPage;
 import com.example.newvpn.utils.ButtonsNavigation;
+import com.example.newvpn.utils.HeaderHelper;
 
 public class LoginPage extends AppCompatActivity {
     
@@ -14,7 +17,10 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_login);
-        
+
+        // Inyectar el header
+        HeaderHelper.injectHeader(this, R.id.fl_login_header_container, "New VPN");
+
         // Encontrar el botón de inicio de sesión
         Button loginButton = findViewById(R.id.bt_session_login_login);
         
@@ -24,6 +30,18 @@ public class LoginPage extends AppCompatActivity {
             public void onClick(View v) {
                 // Navegar a MainPage al hacer clic en el botón
                 ButtonsNavigation.navigateTo(LoginPage.this, MainPage.class);
+            }
+        });
+
+        // Encontrar el TextView de registro
+        TextView registerTextView = findViewById(R.id.tv_session_login_goto_register);
+
+        // Añadir listener para el evento de clic
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navegar a RegisterPage al hacer clic en el TextView
+                ButtonsNavigation.navigateTo(LoginPage.this, RegisterPage.class);
             }
         });
     }
--- app/src/main/java/com/example/newvpn/session/RegisterPage.java
+++ app/src/main/java/com/example/newvpn/session/RegisterPage.java
@@ -3,6 +3,7 @@ package com.example.newvpn.session;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.newvpn.R;
+import com.example.newvpn.utils.HeaderHelper;
 
 public class RegisterPage extends AppCompatActivity {
     
@@ -10,6 +11,9 @@ public class RegisterPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session_register);
+
+        // Inyectar el header
+        HeaderHelper.injectHeader(this, R.id.fl_register_header_container, "New VPN");
     }
 
 }
