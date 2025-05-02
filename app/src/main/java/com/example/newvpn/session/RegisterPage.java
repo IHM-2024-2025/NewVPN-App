3e5b66d976b91983840e615d9fa21da9f99d207a
José Gallardo <tory002254@gmail.com>
2025-05-02 08:46:24 +0200

    Add navigation to login and main pages in RegisterPage, update layout margins in XML

--- app/src/main/java/com/example/newvpn/session/RegisterPage.java
+++ app/src/main/java/com/example/newvpn/session/RegisterPage.java
@@ -1,8 +1,13 @@
 package com.example.newvpn.session;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.newvpn.R;
+import com.example.newvpn.app.MainPage;
 import com.example.newvpn.utils.HeaderHelper;
 
 public class RegisterPage extends AppCompatActivity {
@@ -14,6 +19,29 @@ public class RegisterPage extends AppCompatActivity {
 
         // Inyectar el header
         HeaderHelper.injectHeader(this, R.id.fl_register_header_container, "New VPN");
+        
+        // Configurar el evento para ir a la pantalla de inicio de sesión
+        TextView loginTextView = findViewById(R.id.tv_session_register_goto_login);
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navegar a la pantalla de inicio de sesión
+                Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                startActivity(intent);
+                finish(); // Cerrar la actividad actual
+            }
+        });
+        
+        // Configurar el botón de registro para redirigir a la página principal
+        Button registerButton = findViewById(R.id.bt_session_register_register);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navegar a la pantalla principal
+                Intent intent = new Intent(RegisterPage.this, MainPage.class);
+                startActivity(intent);
+                finish(); // Cerrar la actividad actual
+            }
+        });
     }
-
 }
