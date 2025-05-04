9b2a655ad7688c46986ab5bfe0f2385ca0851de6
tatiana <tbf1003@alu.ubu.es>
2025-05-04 22:36:35 +0200

    Página principal final

--- app/src/main/java/com/example/newvpn/app/MainPage.java
+++ app/src/main/java/com/example/newvpn/app/MainPage.java
@@ -1,6 +1,10 @@
 package com.example.newvpn.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.newvpn.R;
@@ -8,6 +12,10 @@ import com.example.newvpn.utils.HeaderHelper;
 import com.example.newvpn.utils.MenuHelper;
 
 public class MainPage extends AppCompatActivity {
+
+    private boolean isVpnOn = false;
+    private View innerCircle;
+    private View onText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,20 @@ public class MainPage extends AppCompatActivity {
 
         // Inyectar el menú en su contenedor
         MenuHelper.injectMenu(this, R.id.fl_mainpage_menu_container);
+        innerCircle = findViewById(R.id.innerCircle);
+        onText = findViewById(R.id.boton_on);
+        FrameLayout vpnButton = findViewById(R.id.vpnButton);
+
+        vpnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isVpnOn = !isVpnOn;
+                if (isVpnOn) {
+                    innerCircle.setBackgroundResource(R.drawable.circle_green);
+                } else {
+                    innerCircle.setBackgroundResource(R.drawable.circle_dark);
+                }
+            }
+        });
     }
 }
