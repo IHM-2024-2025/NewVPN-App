6edab29685c3bb373cc092ffe980d4f9b9ec26b1
tatiana <tbf1003@alu.ubu.es>
2025-05-13 14:38:21 +0200

    Progreso FaQ

--- app/src/main/java/com/example/newvpn/info/FaqPage.java
+++ app/src/main/java/com/example/newvpn/info/FaqPage.java
@@ -14,17 +14,6 @@ public class FaqPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_faq);
-        View volverFaq = findViewById(R.id.volver_faq);
 
-        // Habilitar el botón
-        volverFaq.setEnabled(true);
-
-        // Acción al hacer clic en el botón
-        volverFaq.setOnClickListener(v -> {
-            // Ir a la actividad ContactPage
-            Intent intent = new Intent(FaqPage.this, ContactPage.class);
-            startActivity(intent);
-            finish(); // Opcional: cerrar FaqPage para que no se pueda volver con el botón "Atrás"
-        });
     }
 }
